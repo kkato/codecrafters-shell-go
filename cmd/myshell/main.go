20c3dbe72cd72576c54045e252da06a22e788094
kkato <kkato149@gmail.com>
2024-12-14 15:54:23 +0900

    Fix

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	for {
-		fmt.Fprint(os.Stdout, "$ ")
+		fmt.Print("$ ")
 
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
@@ -17,17 +17,18 @@ func main() {
 			return
 		}
 
+		command = strings.TrimSpace(command)
 		args := strings.Split(command, " ")
 
 		switch args[0] {
 		case "exit":
-			if args[1] == "0\n" {
+			if args[1] == "0" {
 				os.Exit(0)
 			}
 		case "echo":
 			fmt.Println(strings.Join(args[1:], " "))
 		default:
-			fmt.Println(command[:len(command)-1] + ": command not found")
+			fmt.Println(command + ": command not found")
 		}
 	}
 }
